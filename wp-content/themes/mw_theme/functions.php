eb1a3d54c42cca7c78dc41c467400bdaa114ce72
maryjweise <maryjweise@gmail.com>
2018-04-18 14:31:28 -0700

    Changes relating to Lynda Course: ep 03_04, 03_05, 03_06, 03_07. Allowing translators to control web fonts, preconnecting web fonts for improved performance, working with sass and source maps, creating responsive typography

--- wp-content/themes/mw_theme/functions.php
+++ wp-content/themes/mw_theme/functions.php
@@ -83,6 +83,62 @@ if ( ! function_exists( 'mw_theme_setup' ) ) :
 endif;
 add_action( 'after_setup_theme', 'mw_theme_setup' );
 
+
+function mw_theme_fonts_url() {
+	$fonts_url = '';
+
+	/*
+	 * Translators: If there are characters in your language that are not
+	 * supported by Source Sans Pro and PT Serif, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$source_sans_pro = _x( 'on', 'Source Sans Pro font: on or off', 'mw_theme' );
+        $pt_serif = _x( 'on', 'PT Serif font: on or off', 'mw_theme' );
+        
+        $font_families = array();
+
+        if ( 'off' !== $source_sans_pro ) {
+            $font_families[] = 'Source Sans Pro: 400,400i,700,900';
+        }
+        if ( 'off' !== $pt_serif ) {
+            $font_families[] = 'PT Serif: 400,400i,700,700i';
+        }
+        
+	if ( in_array('on', array($source_sans_pro, $pt_serif))) {
+
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
+
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Seventeen 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function mw_theme_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'mw_theme-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'mw_theme_resource_hints', 10, 2 );
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
@@ -121,7 +177,7 @@ add_action( 'widgets_init', 'mw_theme_widgets_init' );
  */
 function mw_theme_scripts() {
         //Enqueue Google Fonts: Source Sans Pro + PT Serif
-        wp_enqueue_style( 'mw_theme-fonts', 'https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i|Source+Sans+Pro:300,400,400i,600,900');
+        wp_enqueue_style( 'mw_theme-fonts', mw_theme_fonts_url());
 	wp_enqueue_style( 'mw_theme-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'mw_theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
