047fbe713b074434648802709462e86cf1ab4220
maryjweise <maryjweise@gmail.com>
2018-04-19 18:30:48 -0700

    Lesson 04_04: Add an optional custom logo to the header. Made sure it was responsive and header is displayed correctly regardless of which of the 3 elements (logo, title, tagline) are present.

--- wp-content/themes/mw_theme/header.php
+++ wp-content/themes/mw_theme/header.php
@@ -30,9 +30,10 @@
         <?php endif; //end header image check?>
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
+			<?php the_custom_logo();?>
+                        <div class="site-branding__text">
+                        
+			<?php if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
@@ -46,6 +47,7 @@
 				?>
 				<p class="site-description"><?php echo $mw_theme_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
+                    </div><!-- .site-branding__text-->
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
