af6fef9f06ed28579bc1083be705339bf570e4a0
maryjweise <maryjweise@gmail.com>
2018-04-24 07:11:54 -0700

    Lesson 07_04: Use Flexbox to create a responsive layout
    Well FUCK I definitely overwrote my changes like 3 times. ANYWAY, we used really basic flex, and some width variables, in order to create the structure of a full width post header, section of content, comments, post nav to the left, and sidebar to the right. It's all constrained in a max width so it's no longer fluid to the whole site. I'm going to review git on my lunch! FML

--- wp-content/themes/mw_theme/single.php
+++ wp-content/themes/mw_theme/single.php
@@ -1,33 +1,22 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The template for displaying all single posts.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package MW_Theme
  */
 
-get_header();
-?>
+get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main" role="main">
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			get_template_part( 'template-parts/content', 'single' );
 
-                        mw_theme_post_navigation();
-                        
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-                        
-                        
-                        
 		endwhile; // End of the loop.
 		?>
 
@@ -35,5 +24,5 @@ get_header();
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
