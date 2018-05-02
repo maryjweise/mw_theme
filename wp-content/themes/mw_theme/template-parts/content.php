0548e226bd25328f649bbb69ef8c5861589c93d9
maryjweise <maryjweise@gmail.com>
2018-05-02 06:47:59 -0700

    L11_05: Display excerpts in indexes and archives
    remove the_content and replace with the_excerpt
    remove wp_link_pages

--- wp-content/themes/mw_theme/template-parts/content.php
+++ wp-content/themes/mw_theme/template-parts/content.php
@@ -30,23 +30,7 @@
 
 	<div class="entry-content">
 		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'mw_theme' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'mw_theme' ),
-			'after'  => '</div>',
-		) );
+		the_excerpt();
 		?>
 	</div><!-- .entry-content -->
         <div class="continue-reading">
