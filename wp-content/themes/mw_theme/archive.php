e06deefecae298b0077fd11f1758e5357dc3405f
maryjweise <maryjweise@gmail.com>
2018-05-03 14:01:16 -0700

    L11_12: Customize 404 page and empty search results
    Rebuild the Nothing Found system/template
    - update content-none to output the entire page structure
    - in archive and search templates, break from query if there are
    no posts
    - add 6 most recent posts query to content-none
    - also update index.php to work with new content-none

--- wp-content/themes/mw_theme/archive.php
+++ wp-content/themes/mw_theme/archive.php
@@ -9,47 +9,46 @@
 
 get_header();?>
 
-<?php if ( have_posts() ) : ?>
+<?php
+if ( have_posts() ) : ?>
 
-        <header class="page-header">
-                <?php
-                the_archive_title( '<h1 class="page-title">', '</h1>' );
-                the_archive_description( '<div class="archive-description">', '</div>' );
-                ?>
-        </header><!-- .page-header -->
-        
-        <?php endif; ?>
+	<header class="page-header">
+		<?php
+			the_archive_title( '<h1 class="page-title">', '</h1>' );
+			the_archive_description( '<div class="archive-description">', '</div>' );
+		?>
+	</header><!-- .page-header -->
+
+<?php
+else :
+
+	get_template_part( 'template-parts/content', 'none' );
+	return;
+
+endif; ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
+		<main id="main" class="site-main" role="main">
 
 			<?php
 			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			while ( have_posts() ) : the_post();
 
 				/*
-				 * Include the Post-Type-specific template for the content.
+				 * Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content', get_post_format() );
 
 			endwhile;
 
 			the_posts_pagination( array(
-                            'prev_text' => __( 'Newer', 'mw_theme'),
-                            'next_text' => __( 'Older', 'mw_theme'),
-                            'before_page_number' => '<span class="screen-reader-text">' . __( 'Page', 'mw_theme') . '</span>',
-                        ));
+				'prev_text' => __( 'Newer', 'mw_theme' ),
+				'next_text' => __( 'Older', 'mw_theme' ),
+				'before_page_number' => '<span class="screen-reader-text">' . __( 'Page ', 'mw_theme' ) . '</span>',
+			));
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
 		?>
 
 		</main><!-- #main -->
--- wp-content/themes/mw_theme/search.php
+++ wp-content/themes/mw_theme/search.php
@@ -7,49 +7,45 @@
  * @package MW_Theme
  */
 
-get_header();
-?>
-    <?php if ( have_posts() ) : ?>
+get_header(); ?>
 
-        <header class="page-header">
-                <h1 class="page-title">
-                        <?php
-                        /* translators: %s: search query. */
-                        printf( esc_html__( 'Search Results for: %s', 'mw_theme' ), '<span>' . get_search_query() . '</span>' );
-                        ?>
-                </h1>
-        </header><!-- .page-header -->
-    <?php endif;?>
+<?php
+if ( have_posts() ) : ?>
+
+	<header class="page-header">
+		<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'mw_theme' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+	</header><!-- .page-header -->
+
+<?php
+else :
+
+	get_template_part( 'template-parts/content', 'none' );
+	return;
+
+endif; ?>
+
 	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php
+		/* Start the Loop */
+		while ( have_posts() ) : the_post();
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			/**
+			 * Run the loop for the search to output the results.
+			 * If you want to overload this in a child theme then include a file
+			 * called content-search.php and that will be used instead.
+			 */
+			get_template_part( 'template-parts/content' );
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content');
+		endwhile;
 
-			endwhile;
+		the_posts_pagination( array(
+			'prev_text' => __( 'Newer', 'mw_theme' ),
+			'next_text' => __( 'Older', 'mw_theme' ),
+			'before_page_number' => '<span class="screen-reader-text">' . __( 'Page ', 'mw_theme' ) . '</span>',
+		));
 
-			the_posts_pagination( array(
-                            'prev_text' => __( 'Newer', 'mw_theme'),
-                            'next_text' => __( 'Older', 'mw_theme'),
-                            'before_page_number' => '<span class="screen-reader-text">' . __( 'Page', 'mw_theme') . '</span>',
-                        ));
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
 		?>
 
 		</main><!-- #main -->
