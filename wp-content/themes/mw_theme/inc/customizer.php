490c9fc6a0e8e143c3c94b0d34a15db75455ceb2
maryjweise <maryjweise@gmail.com>
2018-05-08 14:21:14 -0700

    L14_04: Apply Customizer changes to the front end
    move customizer style output to customizer.php
    add new rule to apply custom bg color to site header & footer

--- wp-content/themes/mw_theme/inc/customizer.php
+++ wp-content/themes/mw_theme/inc/customizer.php
@@ -78,3 +78,56 @@ function mw_theme_customize_preview_js() {
 	wp_enqueue_script( 'mw_theme-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'mw_theme_customize_preview_js' );
+
+if ( ! function_exists( 'mw_theme_header_style' ) ) :
+	/**
+	 * Styles the header image and text displayed on the blog.
+	 *
+	 * @see mw_theme_custom_header_setup().
+	 */
+	function mw_theme_header_style() {
+		$header_text_color = get_header_textcolor();
+                $header_bg_color = get_theme_mod('theme_bg_color');
+
+		/*
+		 * If no custom options for text are set, let's bail.
+		 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
+		 */
+		if ( get_theme_support( 'custom-header', 'default-text-color' ) != $header_text_color ) {
+
+
+                    // If we get this far, we have custom styles. Let's do this.
+                    ?>
+                    <style type="text/css">
+                    <?php
+                    // Has the text been hidden?
+                    if ( ! display_header_text() ) :
+                            ?>
+                            .site-title,
+                            .site-description {
+                                    position: absolute;
+                                    clip: rect(1px, 1px, 1px, 1px);
+                            }
+                    <?php
+                    // If the user has set a custom color for the text use that.
+                    else :
+                            ?>
+                            .site-title a,
+                            .site-description {
+                                    color: #<?php echo esc_attr( $header_text_color ); ?>;
+                            }
+                    <?php endif; ?>
+                    </style>
+                    <?php
+                }
+            if('#002254' != $header_bg_color) {?>
+                    <style type="text/css">
+                        .site-header,
+                        .site-footer {
+                            background-color: <?php echo esc_attr( $header_bg_color); ?>;
+                        }
+                        
+                    </style>
+            <?php }
+	}
+endif;
