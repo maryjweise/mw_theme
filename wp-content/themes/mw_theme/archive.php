9e279aef76b6dc0e61192869cb3567fe4e3e973c
maryjweise <maryjweise@gmail.com>
2018-05-08 06:31:18 -0700

    L13_03: Add your own SVG icons in your themes
    Use icomoon.io to generate new long arrow icons
    add new icons to img/svg-icons.svg
    Update pagination call to reference new icon IDs
    in index.php, search.php, archive.php

--- wp-content/themes/mw_theme/archive.php
+++ wp-content/themes/mw_theme/archive.php
@@ -44,8 +44,8 @@ endif; ?>
 			endwhile;
 
 			the_posts_pagination( array(
-				'prev_text' => mw_theme_get_svg(array( 'icon' => 'arrow-left')) . __( ' Newer', 'mw_theme' ),
-				'next_text' => __( 'Older ', 'mw_theme' ) . mw_theme_get_svg(array( 'icon' => 'arrow-right')),
+				'prev_text' => mw_theme_get_svg(array( 'icon' => 'arrow-long-left')) . __( ' Newer', 'mw_theme' ),
+				'next_text' => __( 'Older ', 'mw_theme' ) . mw_theme_get_svg(array( 'icon' => 'arrow-long-right')),
 				'before_page_number' => '<span class="screen-reader-text">' . __( 'Page ', 'mw_theme' ) . '</span>',
 			));
 
